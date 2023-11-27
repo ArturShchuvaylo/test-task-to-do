78df360167ade25550b654de195c7a54b138e926
ArturShchuvaylo <arthur.shchuvaylo@gmail.com>
2023-11-27 06:45:08 +0200

    Fetching TODO List from an API:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Асинхронная функция для получения данных с сервера
+    const fetchData = async () => {
+      try {
+        // Выполнение GET-запроса к API
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        // Обработка ошибок при выполнении запроса
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    // Вызов функции fetchData при монтировании компонента
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
